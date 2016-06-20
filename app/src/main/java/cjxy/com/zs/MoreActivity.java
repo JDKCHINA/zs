323d46dc7d9a99deb0bae81cf76927fbe368cb9e
your nameJDKCHINAgit config --global user.name your <935341417@qq.com>
2016-06-20 21:27:20 +0800

    Develop

---
+++ app/src/main/java/cjxy/com/zs/MoreActivity.java
@@ -0,0 +1,15 @@
+package cjxy.com.zs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MoreActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.more_activity);
+	}		
+
+}
